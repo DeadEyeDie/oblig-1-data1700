0df199cd57c50e0a97ba76f8857d8ae7bf392cab
DeadEyeDie <113912677+DeadEyeDie@users.noreply.github.com>
2024-02-16 15:45:22 +0100

    filer

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,5 +1,16 @@
 let billetter = [];
 
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const phonePattern = /^\+(?:\d *){6,14}\d$/;
+
+function validMail(email) {
+    return emailPattern.test(email)
+}
+
+function validNumber(number){
+    return phonePattern.test(number)
+}
+
 function kjop() {
     const film = document.getElementById('film').value;
     const antall = document.getElementById('antall').value;
@@ -10,10 +21,11 @@ function kjop() {
 
 
 
+
 //input validering
 
 
-if (film && antall && fornavn && etternavn && telefon && epost) {
+if (film && antall>0 && fornavn && etternavn && validNumber(telefon) && validMail(epost)) {
     const billett = {
         film: film,
         antall: antall,
@@ -25,9 +37,26 @@ if (film && antall && fornavn && etternavn && telefon && epost) {
     billetter.push(billett);
     visBilletter();
     nullstillInput();
+    }else if(antall>0 && fornavn && etternavn && validNumber(telefon) && validMail(epost)){
+    alert('Vennligst velg en film');
+
+    } else if(film && antall<1 && fornavn && etternavn && validNumber(telefon) && validMail(epost)){
+    alert('Antall billetter må være positivt');
+
+    } else if(film && antall>0 && !fornavn || !etternavn && validNumber(telefon) && validMail(epost)){
+    alert('Vennligst fyll inn både fornavn og etternavn');
+    } else if(film && antall>0 && fornavn && etternavn && !validNumber(telefon) && validMail(epost)){
+    alert('Vennligst bruk riktig format for telefonnummer. Nummeret må inneholde landskode og det må ligge mellom 6 og 14 sifre\n' +
+        'eksempel: "+1 1234567890"');
+    } else if(film && antall>0 && fornavn && etternavn && validNumber(telefon) && !validMail(epost)){
+    alert('Vennligst brukt riktig format for epost.\n' +
+        'eksempel: "hvaSomHelst@hvasomhelst.minstTotegnfraA-Z"');
     }else {
-    alert('Vennligst fyll ut alle feltene.');
-    }
+    alert('Du har glemt eller fylt inn feil på 2 eller flere felt, vennligst sjekk at antall er mer enn 0, at du bruker riktig format.\n' +
+        'Format for telefon: "+1 1234567890"\n' +
+        'Format for epost: hvaSomHelst@hvasomhelst.minstTotegnfraA-Z"')
+}
+
 }
 function slettAlt() {
     billetter = [];
